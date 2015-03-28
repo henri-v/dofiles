ed362dc8b9528cf195180bb9387564ad82411f16
Henri Vettenranta <heto88@gmail.com>
2015-03-28 23:03:39 +0200

    Add initial git configuration

---
+++ git_config.cmd
@@ -0,0 +1,4 @@
+git config --global user.name "Henri Vettenranta"
+git config --global user.email heto88@gmail.com
+git config --global core.editor "cmd //c \"gvim -f\""
+git config --global core.excludesfile "%~dp0gitexcludes"
