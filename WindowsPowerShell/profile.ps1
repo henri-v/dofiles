048beef8b8cf074056275853090737b18ffdc5d8
Henri Vettenranta <heto88@gmail.com>
2016-10-28 02:20:12 +0300

    PS: Complete also on Ctrl+I

--- WindowsPowerShell/profile.ps1
+++ WindowsPowerShell/profile.ps1
@@ -3,5 +3,6 @@ Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineKeyHandler -Key Ctrl+V -Function Paste
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key Ctrl+I -Function Complete
 
 Update-FormatData -PrependPath $PSScriptRoot\FileSystem.format.ps1xml
