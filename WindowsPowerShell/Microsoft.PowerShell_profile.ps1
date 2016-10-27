ad331ea4e1182bb466795441c9f2979070dcabc7
Henri Vettenranta <heto88@gmail.com>
2016-10-27 23:28:49 +0300

    Add PowerShell prompt

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,36 @@
+$isAdmin = (New-Object System.Security.Principal.WindowsPrincipal(
+	[System.Security.Principal.WindowsIdentity]::GetCurrent())).
+	IsInRole("Administrators")
+
+function prompt {
+	# The following should probably be first so that the latter commands
+	# won't trash its results.
+	$exitstatusstr = if ($?) { "" } else {
+		# If we haven't run any external executables yet, LASTEXITCODE
+		# does not seem to exist yet.
+		 if (Test-Path variable:LASTEXITCODE) {
+			" " + $LASTEXITCODE.ToString()
+		} else {
+			" E"
+		}
+	}
+
+	$time = (Get-Date -DisplayHint Time).DateTime
+
+	$curdir = $executionContext.SessionState.Path.CurrentLocation.Path
+	$shortcwd = switch ($curdir) {
+		$env:USERPROFILE { "~" }
+		default {
+			$curdir.Split("\") |
+				Where-Object { -not ($_ -eq "") } |
+				Select-Object -Last 1
+		}
+	}
+
+	$promptchar = if ($isAdmin) { "#" } else { ">" }
+	$promptchars = $promptchar * ($nestedPromptLevel + 1)
+
+	$host.UI.RawUI.WindowTitle = "${env:COMPUTERNAME}: $shortcwd"
+
+	return "[$time] $shortcwd$exitstatusstr$promptchars "
+}
