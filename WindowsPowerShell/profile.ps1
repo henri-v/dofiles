5e34938b650bf258db43c201168556a84bccb63f
Henri Vettenranta <heto88@gmail.com>
2016-10-28 00:39:45 +0300

    Format file system wide by default

--- WindowsPowerShell/profile.ps1
+++ WindowsPowerShell/profile.ps1
@@ -3,3 +3,5 @@ Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineKeyHandler -Key Ctrl+V -Function Paste
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Update-FormatData -PrependPath $PSScriptRoot\FileSystem.format.ps1xml
