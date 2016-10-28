8c9db7416765099d53684648ebb02f8c65fda127
Henri Vettenranta <heto88@gmail.com>
2016-10-28 10:59:52 +0300

    Add git-cdup to PowerShell

--- WindowsPowerShell/profile.ps1
+++ WindowsPowerShell/profile.ps1
@@ -6,3 +6,7 @@ Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineKeyHandler -Key Ctrl+I -Function Complete
 
 Update-FormatData -PrependPath $PSScriptRoot\FileSystem.format.ps1xml
+
+function git-cdup {
+	Set-Location -Path $(git rev-parse --show-cdup)
+}
